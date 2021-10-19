c277eee56ffbc7ec2c2711f2454581f1659fe747
Adem <adem.mahouachi.1s5@gmail.com>
2021-10-19 12:49:24 +0200

    calculer_les_moyennes_des_eleves

---
+++ calculer_les_moyenne_des_eleves.py
@@ -0,0 +1,7 @@
+for i in range (1,21):
+    print ("l'eleves",i)
+    mt1=float(input("saisir mt1"))
+    mt2=float(input("saisir mt2"))
+    mt3=float(input("saisir mt3"))
+    m=(mt1+2*mt2+2*mt3)/5
+    print("la mpyenne de l'eleve n",i,"=",ma)
